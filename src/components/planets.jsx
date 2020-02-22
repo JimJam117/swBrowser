65d5a83789eb800253064aab2f1f087e62cc733c
jimjam117 <jamessparrow101@gmail.com>
2020-02-22 18:53:18 +0000

    Added planets list

--- src/components/planets.jsx
+++ src/components/planets.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export class planets extends Component {
+    constructor() {
+        super();
+        this.state = {
+            loading : true,
+            planets : []
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://swapi.co/api/planets")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    loading : false,
+                    planets : data.results
+                })
+            })
+    }
+
+
     render() {
-        return (
+         return (
             <div>
-                planets
+                <h1>Planets</h1>
+                {this.state.loading ? "Loading" : null}
+                { this.state.planets.map((planet) => 
+                    <Link to = {`/planets/${planet.name}`} key={planet.name}>
+                        <div>
+                            <h3>Name: {planet.name}</h3>
+                            <p>Terrain: {planet.terrain}</p>
+                            <p>Climate: {planet.climate}</p>
+                        </div>
+                    </Link>
+                )}
             </div>
         );
     }
